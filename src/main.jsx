deb798b3d6f72f881ccb123b83bca6956b98ba3f
Samibunleap <samibunleapp@gmail.com>
2026-08-17 13:57:13 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  <BrowserRouter>
-    <AuthProvider>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </AuthProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </StrictMode>
 );
-;
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/WCT-Final-Project-STATIONCOFFEE-/',
+  base: "/WCT-Final-Project-STATIONCOFFEE-/",
 });
